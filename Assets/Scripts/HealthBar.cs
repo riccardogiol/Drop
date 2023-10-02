a37557c30ba29b35e07e9b37ba89d1f25c5bc832
Riccardo Giol <riccardogiol@gmail.com>
2023-10-02 16:03:18 +0200

    health bar modulable + take off tearing from tilemap

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -6,9 +6,40 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Text hp;
 
+    public GameObject barBackgroundImage;
+    public GameObject barMaskImage;
+    public GameObject barBackgroundParent;
+    public GameObject barMaskParent;
+    public GameObject barTop;
+
+    RectTransform rectTransform;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
     public void SetMaxHealth(int maxHealth)
     {
         slider.maxValue = maxHealth;
+        int numIcons = maxHealth/2;
+        rectTransform.sizeDelta = new Vector2(numIcons*50, 100);
+
+        GameObject goRef;
+        for (int i = 0; i < numIcons; i++)
+        {
+            goRef = Instantiate(barBackgroundImage, new Vector2(0, 0), Quaternion.identity);
+            goRef.transform.SetParent(barBackgroundParent.transform);
+            goRef.GetComponent<RectTransform>().anchoredPosition = new Vector2(50*i, 0);
+            goRef.transform.localScale = Vector3.one;
+    
+            goRef = Instantiate(barMaskImage, new Vector2(0, 0), Quaternion.identity);
+            goRef.transform.SetParent(barMaskParent.transform);
+            goRef.GetComponent<RectTransform>().anchoredPosition = new Vector2(50*i, 0);
+            goRef.transform.localScale = Vector3.one;
+        }
+        barTop.GetComponent<RectTransform>().anchoredPosition = new Vector2(50*numIcons, 0);
+
     }
 
     public void SetHealth(int currentHealth)
