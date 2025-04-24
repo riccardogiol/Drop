da0079627c6c22680fd4d0452befde99e57fc41b
Riccardo Giol <riccardogiol@gmail.com>
2025-04-24 16:16:29 +0900

    reshape opening scene

---
+++ Assets/ScaleOnCanvasResize.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ScaleOnCanvasResize : MonoBehaviour
+{
+    public RectTransform canvasRT;
+    public int HPadding = 0;
+    public int VPadding = 0;
+
+    float originalWidth;
+    float originalHeight;
+
+    float currentRatio = 0;
+
+    void Awake()
+    {
+        originalWidth = GetComponent<RectTransform>().rect.width;
+        originalHeight = GetComponent<RectTransform>().rect.height;
+    }
+
+    void Update()
+    {
+        float newRatio = canvasRT.rect.width / canvasRT.rect.height;
+        if (newRatio != currentRatio)
+        {
+            currentRatio = newRatio;
+            if (currentRatio > 1)
+            {
+                float scalingFactor = canvasRT.rect.height/(originalHeight + VPadding*2);
+                GetComponent<RectTransform>().localScale = new Vector3(scalingFactor, scalingFactor, 1); 
+            } else {
+                float scalingFactor = canvasRT.rect.width/(originalWidth + HPadding*2);
+                GetComponent<RectTransform>().localScale = new Vector3(scalingFactor, scalingFactor, 1); 
+            }
+        }
+        
+    }
+}
