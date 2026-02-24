8ea5ced11818e73ef01bde20e24c6d93441058fb
Riccardo Giol <riccardogiol@gmail.com>
2026-02-24 18:48:09 +0100

    setup achievements steam

--- Assets/SteamAchivementManager.cs
+++ Assets/SteamAchivementManager.cs
@@ -1,17 +1,54 @@
 using UnityEngine;
-//using Steamworks;
+using Steamworks;
 
 public class SteamAchivementManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static SteamAchivementManager instance;
+
+    public bool initialized;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        initialized = SteamAPI.Init();
+
+        if (!initialized)
+        {
+            Debug.LogWarning("Steam non inizializzato");
+        }
+    }
     void Start()
     {
-        
+        Debug.Log("Steam App ID: " + SteamUtils.GetAppID());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (initialized)
+            SteamAPI.RunCallbacks();
+    }
+
+    public void UnlockAchievement(string id)
+    {
+        if (!initialized) return;
+
+        SteamUserStats.SetAchievement(id);
+        SteamUserStats.StoreStats();
+    }
+
+    void OnApplicationQuit()
+    {
+        if (initialized)
+        {
+            SteamAPI.Shutdown();
+        }
     }
 }
